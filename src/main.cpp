16711da909b885d2cacc485ef4392d4ac27124eb
Sascha Peilicke <saschpe@gmx.de>
2010-01-27 14:05:28 +0000

    Updated copyright date in kigo docs and it's about data.
    svn path=/trunk/KDE/kdegames/doc/kigo/; revision=1081029

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 {
     KAboutData aboutData("kigo", 0, ki18n("Kigo"), "v0.5.5",
             ki18n("KDE Go Board Game"), KAboutData::License_GPL_V3,
-            ki18n("Copyright (c) 2008,2009 Sascha Peilicke"));
+            ki18n("Copyright (c) 2008-2010 Sascha Peilicke"));
     aboutData.addAuthor(ki18n("Sascha Peilicke (saschpe)"), ki18n("Original author"),
                         "sasch.pe@gmx.de", "http://saschpe.wordpress.com");
     aboutData.addCredit(ki18n("Yuri Chornoivan"), ki18n("Documentation editor"),
