7e40dd21891b0cf215caecd24ffb586cc7bbcd96
Script Kiddy <scripty@kde.org>
2010-08-22 13:02:57 +0000

    extract all needed files
    svn path=/trunk/KDE/kdegames/kigo/; revision=1166674

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find -name \*.rc` >> rc.cpp || exit 11
 $EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 12
+$EXTRACTRC `find -name \*.kcfg` >> rc.cpp || exit 13
 $XGETTEXT -kaliasLocal `find -name \*.cpp -o -name \*.h` rc.cpp -o $podir/kigo.pot
 rm -f rc.cpp
